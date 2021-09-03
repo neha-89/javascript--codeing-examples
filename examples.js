eb7e8cacd010b8def24692eee7d4dbee09f90943
Neha Sontakke <wasnik.neha@gmail.com>
2021-09-03 23:11:01 +0530

    added examples

--- examples.js
+++ examples.js
@@ -138,3 +138,52 @@ for (const value of myGenerator) {
   console.log(value);
 }
 
+// Code 11 :
+
+console.log(0.1 + 0.2 === 0.3);
+
+// Code 12 :
+
+function main(){
+  console.log('A');
+  setTimeout(
+     function print(){ console.log('B'); }
+  ,0);
+  console.log('C');
+}
+main();
+
+// Code 13 :
+
+function foo() {
+  let x = y = 0;
+  x++;
+  y++;
+  return x;
+}
+
+console.log(foo(), typeof x, typeof y);
+
+// Code 14 :
+
+var car = new Vehicle("Honda", "white", "2010", "UK");
+console.log(car);
+
+function Vehicle(model, color, year, country) {
+    this.model = model;
+    this.color = color;
+    this.year = year;
+    this.country = country;
+}
+
+// Code 15 :
+
+console.log(0.1 + 0.2 === 0.3);
+
+// Code 16 :
+
+var y = 1;
+  if (function f(){}) {
+    y += typeof f;
+  }
+  console.log(y);
