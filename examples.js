70a5f4d2d180855badc90c16c42a9acd9af710d2
Neha Sontakke <wasnik.neha@gmail.com>
2021-09-10 22:50:07 +0530

    added examples

--- examples.js
+++ examples.js
@@ -527,3 +527,23 @@ console.log(walk());
 let run = Person.run;
 console.log(run());
 
+// Code 55:
+
+class Vehicle {
+  constructor(name) {
+    this.name = name;
+  }
+
+  start() {
+    console.log(`${this.name} vehicle started`);
+  }
+}
+class Car extends Vehicle {
+  start() {
+    console.log(`${this.name} car started`);
+    super.start();
+  }
+}
+
+const car = new Car('BMW');
+console.log(car.start());
