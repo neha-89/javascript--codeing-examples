7e3b9198983e398a8abc848b241d8f303f002cb5
Neha Sontakke <wasnik.neha@gmail.com>
2022-04-05 21:59:38 +0530

    added more practise examples

--- check.js
+++ check.js
@@ -185,3 +185,101 @@ let challenge = "30 Days of JavaScript";
 //     3 1 3 9 27
 //     4 1 4 16 64
 //     5 1 5 25 125
+
+// 12.  Use __substr__ to slice out the phrase __because because because__ from 
+//the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+let str = "You cannot end a sentence with because because because is a conjunction."
+//console.log(str.indexOf("because"));
+//let strf = str.firstIndexOf("because");
+// console.log(strf);
+// let stre = str.lastIndexOf("because");
+// console.log(substr(strf,stre));
+//console.log(str.substr());
+console.log(str.match(/because/g).toString());
+// ### Exercises: Level 3
+
+// 1. 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word __love__ in this sentence.
+let stt = "Love is the best thing in this world. Some found their love and some are still looking for their love."
+console.log(stt.match(/love/gi));
+// 2. Use __match()__ to count the number of all __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+// 3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+//     ```js
+//         const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+//     ```
+
+// 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.
+
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+// const firstName = "Neha",
+// const lastName = "Sontakke",
+// const country = "India",
+// const city = "Pune",
+// age = 33,
+// isMarried = true,
+// 2. Check if type of '10' is equal to 10
+console.log('10' == 10);
+// 3. Check if parseInt('9.8') is equal to 10
+console.log(parseInt(9.8)==10);
+// 4. Boolean value is either true or false.
+//    1. Write three JavaScript statement which provide truthy value.
+let num = 454;
+let num1 = true;
+let num3 = -454;
+//    2. Write three JavaScript statement which provide falsy value.
+//0,null,false
+
+// 5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+  //  1. 4 > 3 //true
+  //  2. 4 >= 3//false
+  //  3. 4 < 3//false
+  console.log(4 <= 3);
+  //  4. 4 <= 3//false
+  //  5. 4 == 4 //true
+  //  6. 4 === 4//true
+  //  7. 4 != 4//false
+  //  8. 4 !== 4//false
+  //  9. 4 != '4'//false
+  //  10. 4 == '4'//true
+  //  11. 4 === '4'//false
+//    12. Find the length of python and jargon and make a falsy comparison statement.
+
+// 6. Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+//    1. 4 > 3 && 10 < 12//true
+   console.log(!(4 < 3)) //false
+//    3. 4 > 3 || 10 < 12//true
+//    4. 4 > 3 || 10 > 12//true
+//    5. !(4 > 3)//false
+//    6. !(4 < 3)//true
+//    7. !(false)//true
+//    8. !(4 > 3 && 10 < 12)//false
+//    9. !(4 > 3 && 10 > 12)//true
+//    10. !(4 === '4')//true
+//    11. There is no 'on' in both dragon and python
+
+// 7. Use the Date object to do the following activities
+//    1. What is the year today?
+let now = new Date();
+console.log(now.getFullYear());
+//    2. What is the month today as a number?
+console.log(now.getMonth());
+//    3. What is the date today?
+console.log(now.getDate());
+//    4. What is the day today as a number?
+console.log(now.getDay());
+//    5. What is the hours now?
+console.log(now.getHours());
+//    6. What is the minutes now?
+console.log(now.getMinutes());
+//    7. Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(now.getTime(1-1-1970));
+
+// 1. Create a human readable time format using the Date time object
+//    1. YYYY-MM-DD HH:mm
+//    2. DD-MM-YYYY HH:mm
+//    3. DD/MM/YYYY HH:mm
+console.log(now.toLocaleDateString())
+// console.log(date.toLocaleDateString());
+// console.log(date.toDateString());
+// console.log(date.toGMTString());
--- examples.js
+++ examples.js
@@ -1575,3 +1575,77 @@ let ten = countries.slice(0,10);
 // let count = cou - 1;
 // let middle = countries.slice(count)
 // 2. Divide the countries array into two equal arrays if it is even.  If countries array is not even , one more country for the first half.
+
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+// const firstName = "Neha",
+// const lastName = "Sontakke",
+// const country = "India",
+// const city = "Pune",
+// age = 33,
+// isMarried = true,
+// 2. Check if type of '10' is equal to 10
+console.log('10' == 10);
+// 3. Check if parseInt('9.8') is equal to 10
+console.log(parseInt(9.8)==10);
+// 4. Boolean value is either true or false.
+//    1. Write three JavaScript statement which provide truthy value.
+let num = 454;
+let num1 = true;
+let num3 = -454;
+//    2. Write three JavaScript statement which provide falsy value.
+//0,null,false
+
+// 5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+  //  1. 4 > 3 //true
+  //  2. 4 >= 3//false
+  //  3. 4 < 3//false
+  console.log(4 <= 3);
+  //  4. 4 <= 3//false
+  //  5. 4 == 4 //true
+  //  6. 4 === 4//true
+  //  7. 4 != 4//false
+  //  8. 4 !== 4//false
+  //  9. 4 != '4'//false
+  //  10. 4 == '4'//true
+  //  11. 4 === '4'//false
+//    12. Find the length of python and jargon and make a falsy comparison statement.
+
+// 6. Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+//    1. 4 > 3 && 10 < 12//true
+   console.log(!(4 < 3)) //false
+//    3. 4 > 3 || 10 < 12//true
+//    4. 4 > 3 || 10 > 12//true
+//    5. !(4 > 3)//false
+//    6. !(4 < 3)//true
+//    7. !(false)//true
+//    8. !(4 > 3 && 10 < 12)//false
+//    9. !(4 > 3 && 10 > 12)//true
+//    10. !(4 === '4')//true
+//    11. There is no 'on' in both dragon and python
+
+// 7. Use the Date object to do the following activities
+//    1. What is the year today?
+let now = new Date();
+console.log(now.getFullYear());
+//    2. What is the month today as a number?
+console.log(now.getMonth());
+//    3. What is the date today?
+console.log(now.getDate());
+//    4. What is the day today as a number?
+console.log(now.getDay());
+//    5. What is the hours now?
+console.log(now.getHours());
+//    6. What is the minutes now?
+console.log(now.getMinutes());
+//    7. Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(now.getTime(1-1-1970));
+
+// 1. Create a human readable time format using the Date time object
+//    1. YYYY-MM-DD HH:mm
+//    2. DD-MM-YYYY HH:mm
+//    3. DD/MM/YYYY HH:mm
+console.log(now.toLocaleDateString())
+// console.log(date.toLocaleDateString());
+// console.log(date.toDateString());
+// console.log(date.toGMTString());
